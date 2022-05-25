4142147e666c9c772ae8ec9aef3bb427d0181a18
kuzichkin2001 <kuzichkin.p@yandex.ru>
2022-05-25 14:06:24 +0400

    initial commit.

---
+++ DAL/ProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using Entities;
+using System.Data.SqlClient;
+using Dapper;
+using System.Linq;
+
+namespace DAL;
+public static class ProductRepository
+{
+    private static readonly string _connectionString = @"Server=localhost;Database=Restaurant;UserId=sa;Password=igumug16";
+
+    public static IEnumerable<Product> GetProducts()
+    {
+        using (var conn = new SqlConnection(_connectionString))
+        {
+            var query = "SELECT * FROM Product";
+
+            return conn.Query<Product>(query).ToList();
+        }
+    }
+}
+
---
+++ Entities/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace Entities
+{
+    public class Product
+    {
+        public long Id_Product { get; set; }
+        public string Product_Name { get; set; }
+        public int Exp_Time { get; set; }
+        public DateTime Arrivement_Date { get; set; }
+        public int Cost { get; set; }
+        public string Supplier { get; set; }
+    }
+}
+
