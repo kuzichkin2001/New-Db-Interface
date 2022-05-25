d96b39bbbd93f1bc7edb990fb64eacabb4f3494a
kuzichkin2001 <kuzichkin.p@yandex.ru>
2022-05-25 14:05:05 +0400

    Короче, это интерфейс

---
+++ DB-Interface/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DB_Interface.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
